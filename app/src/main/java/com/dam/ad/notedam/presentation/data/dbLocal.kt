68929eff6692e0720d20edb4d1f425799421b24c
tabat <tabatarov.12@gmail.com>
2023-11-15 12:52:52 +0100

    hecho por Piegpt

---
+++ app/src/main/java/com/dam/ad/notedam/presentation/data/dbLocal.kt
@@ -0,0 +1,65 @@
+package com.dam.ad.notedam.presentation.data
+
+import android.content.Context
+import org.json.JSONObject
+import java.io.BufferedReader
+import java.io.File
+import java.io.FileInputStream
+import java.io.FileOutputStream
+import java.io.IOException
+import java.io.InputStreamReader
+
+class dbLocal(private val context: Context) {
+
+    private val FILE_NAME = "nombre_del_archivo.json"
+    private val PREF_NAME = "nombre_pref"
+    private val PREF_KEY_JSON = "clave_json"
+
+    fun cargarDatos(): String? {
+        val file = File(context.filesDir, FILE_NAME)
+
+        return if (file.exists()) {
+            // El archivo JSON existe, cargarlo
+            try {
+                val fis = FileInputStream(file)
+                val br = BufferedReader(InputStreamReader(fis))
+
+                val jsonStringBuilder = StringBuilder()
+                var line: String?
+
+                while (br.readLine().also { line = it } != null) {
+                    jsonStringBuilder.append(line)
+                }
+
+                fis.close()
+                jsonStringBuilder.toString()
+            } catch (e: IOException) {
+                e.printStackTrace()
+                null
+            }
+        } else {
+            // El archivo no existe
+            null
+        }
+    }
+
+    fun guardarDatos(nuevoJson: JSONObject) {
+        // Convertir el nuevo JSON a String
+        val nuevoJsonString = nuevoJson.toString()
+
+        // Guardar el nuevo JSON en SharedPreferences
+        val editor = context.getSharedPreferences(PREF_NAME, Context.MODE_PRIVATE).edit()
+        editor.putString(PREF_KEY_JSON, nuevoJsonString)
+        editor.apply()
+
+        // Guardar el nuevo JSON también en el archivo local
+        try {
+            val file = File(context.filesDir, FILE_NAME)
+            val fos = FileOutputStream(file)
+            fos.write(nuevoJsonString.toByteArray())
+            fos.close()
+        } catch (e: IOException) {
+            e.printStackTrace()
+        }
+    }
+}
--- app/src/main/java/com/dam/ad/notedam/presentation/home/MainActivity.kt
+++ app/src/main/java/com/dam/ad/notedam/presentation/home/MainActivity.kt
@@ -8,18 +8,39 @@ import androidx.navigation.ui.setupWithNavController
 import com.dam.ad.notedam.R
 import com.dam.ad.notedam.databinding.ActivityMainBinding
 import dagger.hilt.android.AndroidEntryPoint
+import org.json.JSONObject
+import java.io.BufferedReader
+import java.io.File
+import java.io.FileInputStream
+import java.io.InputStreamReader
 
 @AndroidEntryPoint
 class MainActivity : AppCompatActivity() {
 
     private lateinit var binding: ActivityMainBinding
-    private lateinit var navController:NavController
+    private lateinit var navController: NavController
+
+    private val FILE_NAME = "nombre_del_archivo.json"
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
         initNavMenu()
+
+        // Comprobar si hay un archivo JSON
+        val jsonContent = cargarDatos()
+
+        if (jsonContent != null) {
+            // Si hay un archivo JSON, cargarlo
+            // Realiza acciones necesarias con el contenido cargado
+            // ...
+        } else {
+            // Si no hay un archivo JSON, crear uno nuevo
+            val nuevoJson = JSONObject()
+            nuevoJson.put("clave", "valor") // Agrega datos según tus necesidades
+            guardarDatos(nuevoJson)
+        }
     }
 
     private fun initNavMenu() {
@@ -28,4 +49,44 @@ class MainActivity : AppCompatActivity() {
         binding.bottomNavView.setupWithNavController(navController)
     }
 
+    private fun cargarDatos(): String? {
+        val file = File(filesDir, FILE_NAME)
+
+        return if (file.exists()) {
+            // El archivo JSON existe, cargarlo
+            try {
+                val fis = FileInputStream(file)
+                val br = BufferedReader(InputStreamReader(fis))
+
+                val jsonStringBuilder = StringBuilder()
+                var line: String?
+
+                while (br.readLine().also { line = it } != null) {
+                    jsonStringBuilder.append(line)
+                }
+
+                fis.close()
+                jsonStringBuilder.toString()
+            } catch (e: Exception) {
+                e.printStackTrace()
+                null
+            }
+        } else {
+            // El archivo no existe
+            null
+        }
+    }
+
+    private fun guardarDatos(nuevoJson: JSONObject) {
+        // Convertir el nuevo JSON a String
+        val nuevoJsonString = nuevoJson.toString()
+
+        // Guardar el nuevo JSON en el archivo local
+        try {
+            val file = File(filesDir, FILE_NAME)
+            file.writeText(nuevoJsonString)
+        } catch (e: Exception) {
+            e.printStackTrace()
+        }
+    }
 }
